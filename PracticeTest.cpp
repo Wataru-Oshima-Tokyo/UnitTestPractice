a69ee7795564f82e273de3e9b9bdd1cb51fca617
Wataru <wataruoshima@my.smccd.edu>
2022-04-18 11:43:05 -0700

    added test cases

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -30,16 +30,19 @@ TEST(PracticeTest, smoke_test)
 TEST(PracticeTest, zzz_test)
 {
 	Practice prc;
-	int a=3;
-	int b=4;
+	int a=4;
+	int b=5;
 	int c=5;
 	bool t=true;
 	prc.sortDescending(a,b,c);
+	
 	ASSERT_EQ(a,5);
-	ASSERT_EQ(b,4);
-	ASSERT_EQ(c,3);
+	ASSERT_EQ(b,5);
+	ASSERT_EQ(c,4);
+	ASSERT_EQ(prc.count_starting_repeats(""),0);
 
 
-	ASSERT_EQ(prc.isPalindrome("zzz"),t);
-// 	ASSERT_EQ(prc.isPalindrome("zzz"),1);
+	ASSERT_EQ(prc.isPalindrome("zawwAz"),t);
+	ASSERT_EQ(prc.isPalindrome("galkjgalsklal"),!t);
+
 }
